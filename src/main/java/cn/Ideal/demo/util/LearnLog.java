669d1114b60d236c88cbf70d918a4772d9ff0862
wwwwy1 <840985277@qq.com>
2019-11-06 19:23:30 +0800

    对公司接口熟悉,开发流程

--- src/main/java/cn/Ideal/demo/util/LearnLog.java
+++ src/main/java/cn/Ideal/demo/util/LearnLog.java
@@ -65,7 +65,9 @@ public enum  LearnLog {
 	LearnConcurrent23("2019/11/02","利用WordPress搭建自己的博客网站，日常leetcode刷题，主要是格雷码(上周周赛，没做出来的循环码排列get√)"),
 	LearnConcurrent24("2019/11/03","leetcode161周赛，题目好像有点简单，但是比赛中只做出一题，还有就是最后的裴蜀定理是什么鬼（更新LeetCode161周赛4道题）"),
 	LearnConcurrent25("2019/11/04","日常leetcode以及pat一题(反转链表)"),
-	LearnConcurrent26("2019/11/05","日常leetcode(39.组合综合-dfs、56.合并区间)");
+	LearnConcurrent26("2019/11/05","日常leetcode(39.组合综合-dfs、56.合并区间)"),
+	LearnConcurrent27("2019/11/06","对公司接口熟悉,开发流程");
+
 
 
 
