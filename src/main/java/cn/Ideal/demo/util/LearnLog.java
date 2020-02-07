8b551b03b4c7e6812e8c80f96e657c000cfee195
wwwwy1 <840985277@qq.com>
2020-02-07 20:30:41 +0800

    学习并使用SweetAlert

--- src/main/java/cn/Ideal/demo/util/LearnLog.java
+++ src/main/java/cn/Ideal/demo/util/LearnLog.java
@@ -141,7 +141,9 @@ public enum  LearnLog {
 	LearnConcurrent87("2020/02/03","办理离职事项"),
 	LearnConcurrent88("2020/02/04","学习设计模式-建造者模式-原型模式"),
 	LearnConcurrent89("2020/02/05","完成爬虫工具(爬取51job招聘信息)"),
-	LearnConcurrent90("2020/02/06","完善每日一言功能，以及思考总体架构方向");
+	LearnConcurrent90("2020/02/06","完善每日一言功能，以及思考总体架构方向"),
+	LearnConcurrent91("2020/02/07","学习并使用SweetAlert");
+
 
 	// 时间
 	private final String learnTime;
