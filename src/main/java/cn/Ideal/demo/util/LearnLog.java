c7cbf885d2acbed4122f1e9c07a16a1992f9fe57
wwwwy1 <840985277@qq.com>
2020-03-10 19:15:37 +0800

    今日工作：修改部分页面，思考工作

--- src/main/java/cn/Ideal/demo/util/LearnLog.java
+++ src/main/java/cn/Ideal/demo/util/LearnLog.java
@@ -176,7 +176,8 @@ public enum  LearnLog {
 	LearnConcurrent119("2020/03/06","找到合适的个人中心页面，完成用户信息管理页面"),
 	LearnConcurrent120("2020/03/07","完成前端任务清单页面，复习面试题"),
 	LearnConcurrent121("2020/03/08","doing任务清单以及周赛"),
-	LearnConcurrent122("2020/03/09","完成任务清单功能");
+	LearnConcurrent122("2020/03/09","完成任务清单功能"),
+	LearnConcurrent123("2020/03/10","修改部分页面，思考工作");
 	// 时间
 	private final String learnTime;
 	// 内容
