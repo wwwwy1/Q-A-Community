af2fea78e10fa0bed232630e5b01ca56d74d10fb
wwwwy1 <840985277@qq.com>
2020-04-11 22:09:54 +0800

    今日工作:复习面试题(数据库索引、hashmap)

--- src/test/java/cn/Ideal/demo/DemoApplicationTests.java
+++ src/test/java/cn/Ideal/demo/DemoApplicationTests.java
@@ -6055,6 +6055,41 @@ class Trie {
 		}
 		return ans.toString();
 	}
+	public int superEggDrop(int K, int N) {
+		return dp887(K, N);
+	}
+	Map<Integer, Integer> memo = new HashMap();
+	public int dp887(int K, int N) {
+		if (!memo.containsKey(N * 100 + K)) {
+			int ans;
+			if (N == 0)
+				ans = 0;
+			else if (K == 1)
+				ans = N;
+			else {
+				int lo = 1, hi = N;
+				while (lo + 1 < hi) {
+					int x = (lo + hi) / 2;
+					int t1 = dp887(K-1, x-1);
+					int t2 = dp887(K, N-x);
+
+					if (t1 < t2)
+						lo = x;
+					else if (t1 > t2)
+						hi = x;
+					else
+						lo = hi = x;
+				}
+
+				ans = 1 + Math.min(Math.max(dp887(K-1, lo-1), dp887(K, N-lo)),
+						Math.max(dp887(K-1, hi-1), dp887(K, N-hi)));
+			}
+
+			memo.put(N * 100 + K, ans);
+		}
+
+		return memo.get(N * 100 + K);
+	}
 	/*<pre><code class="language-java line-numbers">代码内容</code></pre>*/
 	public  static void main(String[] args) {
 		DemoApplicationTests t = new DemoApplicationTests();
