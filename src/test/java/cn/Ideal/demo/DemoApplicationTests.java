2bcb810d9014a7ffdbed5e85c5e5c546f4759903
wwwwy <usx16147220@163.com>
2022-06-08 15:33:28 +0800

    test:20220608每日一题

--- src/test/java/cn/Ideal/demo/DemoApplicationTests.java
+++ src/test/java/cn/Ideal/demo/DemoApplicationTests.java
@@ -7112,6 +7112,13 @@ class Trie {
 		dict.forEach((k,v)->nums[v]=k);
 		return nums;
 	}
+	//20220608 每日一题
+	public boolean isBoomerang(int[][] points) {
+		int x1 = points[0][0], y1 = points[0][1];
+		int x2 = points[1][0], y2 = points[1][1];
+		int x3 = points[2][0], y3 = points[2][1];
+		return (y2 - y1) * (x3 - x2) != (y3 - y2) * (x2 - x1);
+	}
 	/*<pre><code class="language-java line-numbers">代码内容</code></pre>*/
 	public  static void main(String[] args) {
 		DemoApplicationTests t = new DemoApplicationTests();
